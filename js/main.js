9ae94db46fe27b3439c24e05ddac223729308143
djolekrsticc <djkrstic9@gmail.com>
2023-06-23 19:41:57 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // File name in contact form
 function filename() {
     fu1Value = document.getElementById("file-input").value
-    document.getElementById('file').innerHTML = fu1Value
+    document.getElementById('file').innerHTML = fu1Value.slice(0, 20) + '...' + `<span class="tooltiptext">${fu1Value}</span>`
 }
 
 let fu1 = document.getElementById("file-input");
